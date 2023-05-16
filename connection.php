6ec15742d7fec495fccf39ef8b7627f995362588
PhaiPhattarapong <paikung01@gmail.com>
2023-05-16 22:37:38 +0700

    create connection.php

--- connection.php
+++ connection.php
@@ -1,3 +1,14 @@
 <?php
+$servername = "localhost";
+$username = "username";
+$password = "";
+$database = "loginadminuser";
 
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
 ?>
