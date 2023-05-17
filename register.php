44836d3500434790f2b8d35e37f6a515e355bd4d
PhaiPhattarapong <paikung01@gmail.com>
2023-05-17 14:55:23 +0700

    create form

---
+++ register.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Register</title>
+</head>
+<body>
+    <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+        <label for="usaername">Username</label>
+        <input type="text" name="username" placeholder="Enter username" required>
+        <br>
+        <label for="password">Password</label>
+        <input type="password" name="password" placeholder="Enter password" required>
+        <br>
+        <label for="usaername">Username</label>
+        <input type="text" name="username" placeholder="Enter username" required>
+        <br>
+        <label for="usaername">Username</label>
+        <input type="text" name="username" placeholder="Enter username" required>
+        <br>
+    </form>
+</body>
+</html>
