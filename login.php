d037c5f00e3c6ce24928048fead508ccd92b935a
PhaiPhattarapong <paikung01@gmail.com>
2023-05-18 01:16:16 +0700

    Update login.php

--- login.php
+++ login.php
@@ -3,6 +3,13 @@
 
     if (isset($_POST['username'])) {
         include('connection.php');
+
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $passwordenc = md5($password);
+
+        $query = "SELECT * FROM user WHERE username = '$username' AND password = '$passwordenc'";
+        $result = mysqli_query($conn, $query);
     }
 
 
