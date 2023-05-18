61bfd1c0843a30ae254e4e57b8f8fba0a731c0c9
PhaiPhattarapong <paikung01@gmail.com>
2023-05-18 13:52:42 +0700

    create user.php and update login

--- login.php
+++ login.php
@@ -10,6 +10,26 @@
 
         $query = "SELECT * FROM user WHERE username = '$username' AND password = '$passwordenc'";
         $result = mysqli_query($conn, $query);
+
+        if (mysqli_num_rows($result) == 1) {
+
+            $row = mysqli_fetch_array($result);
+
+            $_SESSION['userid'] = $row['id'];
+            $_SESSION['user'] = $row['firstname'] . "" . $row['lastname'];
+            $_SESSION['userlevel'] = $row['userlevel'];
+
+            if ($_SESSION['userlevel'] == 'a') {
+                header("Location: admin.php");
+            }
+            if ($_SESSION['userlevel'] == 'm') {
+                header("Location: user.php");
+            }
+        } else {
+            echo "<script>alert('User หรือ Password ไม่ถูกต้อง')</script>";
+        }
+    } else {
+        header("Location: index.php");
     }
 
 
